cd5e146b92e6350e4546728a41fe023a211ae6f9
Musa Harb <musa.harb@gmail.com>
2024-02-23 05:23:48 -0800

    added sort results function

--- app.js
+++ app.js
@@ -4,19 +4,54 @@ const app = express();
 
 const PORT = 50000;
 
-async function searchBooks(keyword) {
-  const url = `https://openlibrary.org/search.json?q=${keyword}`;
-  const options = { method: "GET" };
+const MAXRESULTS = 3; // 40 is max allowed by Google Books api, change this number to the number of results you want up to 40.
 
-  let response = await fetch(url, options);
-  const books = await response.json();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+async function searchBooks(keyword) {
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${keyword}&maxResults=${MAXRESULTS}`;
+  const options = { method: "GET" };
+  const response = await fetch(url, options);
+  const searchResults = await response.json();
+  console.dir(searchResults.items);
+  return searchResults;
+}
+
+//This function takes the books search results (the items property)  
+//and put it in an arr of objects with the following properties,
+//feel free to edit or delete this function if you prefer to use
+//the search results directly and edit it on your end.
+function sortResult(data) {
+  const books = [];
+  for (const book of data) {
+    console.log(book.volumeInfo.industryIdentifiers, book.volumeInfo.title);
+    books.push({
+      title: book.volumeInfo.title,
+      author: book.volumeInfo.authors,
+      description: book.volumeInfo.description,
+      thumbnail: book.volumeInfo.imageLinks,                    // arr of objects containing links to book cover images of diff sizes
+      industryIdentifiers: book.volumeInfo.industryIdentifiers, //arr of objects containing ISBN 10 and 13, old books have PSU #
+      infoLink: book.volumeInfo.infoLink,
+      language: book.volumeInfo.language,
+      publishedDate: book.volumeInfo.publishedDate,
+      publisher: book.volumeInfo.publisher,
+      categories: book.volumeInfo.categories,
+      snippet: book.searchInfo.textSnippet,
+    });
+  }
+  console.log(books);
   return books;
 }
 
 app.get("/", async (req, res) => {
   const keyword = req.query.keyword;
-  const books = await searchBooks(keyword);
-  res.json(books.docs[0]);
+  const searchResults = await searchBooks(keyword);
+  const books = sortResult(searchResults.items);
+  res.json(books);
 });
 
 app.listen(PORT);
